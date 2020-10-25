76a4fd9e99dc4853b04c91cb4b5a3dfbee7a767c
Анна <shesterikofff@mail.ru>
2020-10-25 16:29:23 +0300

    Добавила REAME

--- src/Form/Type/BookType.php
+++ src/Form/Type/BookType.php
@@ -6,7 +6,6 @@ namespace App\Form\Type;
 
 use App\Entity\Book;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
